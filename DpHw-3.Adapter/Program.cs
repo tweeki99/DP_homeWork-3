a15a2a6ba47858f218261a4a2b79486033d202b5
Данил Шипилов <danil.shipilov.99@gmail.com>
2019-07-09 23:33:03 +0600

    DpHw-3

---
+++ DpHw-3.Adapter/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace DpHw_3.Adapter
+{
+    public interface ITarget
+    {
+        string Go();
+    }
+    
+    // Класс машины который нужно адаптировать
+    class Car
+    {
+        public string Drive()
+        {
+            return "Машина едет";
+        }
+    }
+    
+    // Адаптер для класса машины. Адаптирует его под общий интерфейс ITarget
+    class CarAdapter : ITarget
+    {
+        private readonly Car _сar;
+
+        public CarAdapter(Car сar)
+        {
+            this._сar = сar;
+        }
+
+        public string Go()
+        {
+            return $"CarAdapter: '{this._сar.Drive()}'";
+        }
+    }
+
+    // Класс самолета который нужно адаптировать
+    class Plane
+    {
+        public string Fly()
+        {
+            return "Самолет летит";
+        }
+    }
+    
+    // Адаптер для класса самолета. Адаптирует его под общий интерфейс ITarget
+    class PlaneAdapter : ITarget
+    {
+        private readonly Plane _plane;
+
+        public PlaneAdapter(Plane plane)
+        {
+            this._plane = plane;
+        }
+
+        public string Go()
+        {
+            return $"PlaneAdapter: '{this._plane.Fly()}'";
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создаем два несовместимых класса
+            Car car = new Car();
+            Plane plane = new Plane();
+
+            // Адаптируем их к одному интерфесу
+            ITarget targetCar = new CarAdapter(car);
+            ITarget targetPlane = new PlaneAdapter(plane);
+
+            // Лицезреем плоды наших трудов
+            Process(targetCar);
+            Process(targetPlane);
+
+            Console.ReadLine();
+        }
+
+        static void Process(ITarget target)
+        {
+            Console.WriteLine(target.Go());
+        }
+    }
+}
---
+++ DpHw-3.Facade/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace DpHw_3.Facade
+{
+    // Создал 3 класса. Каждый совершает определенную операцию над числом
+
+    // Отображает количество знаков в числе
+    public class AmountOfNumbers
+    {
+        public void Operation(int number)
+        {
+            int digitCount = (int)Math.Log10(number) + 1;
+
+            Console.WriteLine("Количество знаков: " + digitCount);
+        }
+    }
+
+    // Отображает сумму знаков в числе
+    public class SumOfNumbers
+    {
+        public void Operation(int number)
+        {
+            int n = Math.Abs(number);
+            int sum = 0;
+            while (n != 0)
+            {
+                sum += n % 10;
+                n /= 10;
+            }
+            Console.WriteLine("Сумма знаков: " + sum);
+        }
+    }
+
+    // Отображает квадрат числа
+    public class SquareOfNumber
+    {
+        public void Operation(int number)
+        {
+            Console.WriteLine("Квадрат числа: " + (number * number));
+        }
+    }
+
+    // Фасад позволяет объединить все эти действия и совершить все операции над числом в одном вызове
+    public class Facade
+    {
+        protected AmountOfNumbers _amountOfNumbers;
+
+        protected SumOfNumbers _sumOfNumbers;
+
+        protected SquareOfNumber _squareOfNumber;
+
+        public Facade(AmountOfNumbers amountOfNumbers, SumOfNumbers sumOfNumbers, SquareOfNumber squareOfNumber)
+        {
+            this._amountOfNumbers = amountOfNumbers;
+            this._sumOfNumbers = sumOfNumbers;
+            this._squareOfNumber = squareOfNumber;
+        }
+
+        public void Operation(int number)
+        {
+            Console.WriteLine("Число: " + number);
+            _amountOfNumbers.Operation(number);
+            _sumOfNumbers.Operation(number);
+            _squareOfNumber.Operation(number);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создаем экземпляры классов
+            AmountOfNumbers amountOfNumbers = new AmountOfNumbers();
+            SumOfNumbers sumOfNumbers = new SumOfNumbers();
+            SquareOfNumber squareOfNumber = new SquareOfNumber();
+
+            // Объеденяем их
+            Facade facade = new Facade(amountOfNumbers, sumOfNumbers, squareOfNumber);
+
+            // Запускаем все методы на исполнение одной строчкой
+            facade.Operation(12345);
+
+            Console.ReadLine();
+        }
+    }
+}
